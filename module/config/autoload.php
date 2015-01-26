ec5c216c038375c6952c35dfc2aa9e890e25a33a
David Molineus <david.molineus@netzmacht.de>
2015-01-26 15:52:41 +0100

    Add quick link templates.

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -11,6 +11,8 @@ TemplateLoader::addFiles(
     array (
         'mod_navbar'             => 'system/modules/bootstrap-navigation/templates',
         'mod_navbar_container'   => 'system/modules/bootstrap-navigation/templates',
+        'mod_quicknav'           => 'system/modules/bootstrap-navigation/templates',
+        'mod_quicklink'          => 'system/modules/bootstrap-navigation/templates',
         'navbar'                 => 'system/modules/bootstrap-navigation/templates',
         'navbar_brand'           => 'system/modules/bootstrap-navigation/templates',
         'navbar_toggle'          => 'system/modules/bootstrap-navigation/templates',
