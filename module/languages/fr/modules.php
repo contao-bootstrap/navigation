64d3ccc88b96ecd38afc475d73ec3c25bca74dbc
David Molineus <david.molineus@netzmacht.de>
2017-01-24 13:48:00 +0100

    Update translations.

---
+++ module/languages/fr/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2017-01-24T13:46:27+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['FMD']['bootstrap_navbar']['0'] = 'Menu de navigation (Bootstrap)';
+$GLOBALS['TL_LANG']['FMD']['bootstrap_navbar']['1'] = 'Élément navbar de Bootstrap';
+
---
+++ module/languages/fr/tl_module.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2017-01-24T13:46:27+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_addHeader']['0']              = 'Ajouter une entête de page';
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_isResponsive']['0']           = 'Responsive';
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_navClass']['0']               = 'Classes de navigation Bootstrap';
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_navbarModules']['0']          = 'Modules';
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_navbarModules_cssClass']['0'] = 'Classe CSS';
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_navbarModules_floating']['0'] = 'Alignement';
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_navbarModules_inactive']['0'] = 'Inactif';
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_navbarModules_inactive']['1'] = 'Désactiver le module temporairement.';
+$GLOBALS['TL_LANG']['tl_module']['bootstrap_navbarTemplate']['0']         = 'Modèle';
+
